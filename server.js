017079a0f0b8d4e770df6ede113207138e60e587
StefanDsd <stefan_dascalescu92@gmail.com>
2022-11-24 15:39:48 +0100

    Routes created

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const { server_port } = process.env;
+
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send("Welcome to the main route");
+})
+
+app.get("/products",(req,res) => {
+    res.send("products route");
+})
+
+app.listen(server_port, (e) => {
+    if(e) {
+        console.log(e);
+    } else {
+        console.log("listening to port", server_port);
+    }
+});
